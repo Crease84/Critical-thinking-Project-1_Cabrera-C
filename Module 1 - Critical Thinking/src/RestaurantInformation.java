8b72e9c2aba1f03829133db1ded7e5f128b26ec4
ChrisTopher C <91694954+Crease84@users.noreply.github.com>
2025-11-01 15:57:59 -0600

    Corrected RestaurantInformation.java
    Added variables to print the information

---
+++ Module 1 - Critical Thinking/src/RestaurantInformation.java
@@ -0,0 +1,23 @@
+/*
+ * Christopher Cabrera
+ * September 14, 2025
+ * CSUGlobal CSC320-1 Programming I
+ * Restaurant Name and Address
+ * This program prints out the name and location of a restaurant I like
+ */
+
+public class RestaurantInformation {
+
+	public static void main(String[] args) {
+		
+		String restaurantName = "Gregorio's";
+		String streetAddress = "1910 Wasdwroth Blvd";
+		String cityStateZip = "Lakewood, CO 80214";
+
+		System.out.println(restaurantName);
+		System.out.println(streetAddress);
+		System.out.println(cityStateZip);
+		
+	}
+
+}
